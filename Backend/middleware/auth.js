d296f731a6ed5f194814e1f03f8597cdf118bd84
Simon <simon_froger@yahoo.fr>
2021-07-22 17:28:28 +0200

    Midleware auth, multer-config

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+  try{
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+        throw 'user ID non valable !';
+    } else {
+        next();
+    }
+  } catch (error) {
+      res.status(401).json({ error: error | 'Requête non authentifié' })
+  }
+}
