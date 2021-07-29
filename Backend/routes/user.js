e8374cf7080a1bd6f7643586086fff62c251f1ea
Simon <simon_froger@yahoo.fr>
2021-07-29 15:56:05 +0200

    Users routes

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -6,6 +6,7 @@ const auth = require('../middleware/auth');
 //Routage
 router.post('/signup',      auth, userCtrl.signup);
 router.post('/login',       auth, userCtrl.login);
+router.get('/allProfile',   auth, userCtrl.getAllProfile)
 router.get('/profile',      auth, userCtrl.userProfile);
 router.delete('/profile',   auth, userCtrl.deleteProfile)
 
